d2a2e0f86ce4075cee8b5b1274ce9819a87d0203
Sven Fuchs <me@svenfuchs.com>
2015-07-26 19:26:59 +0200

    split specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ ENV['ENV'] = 'test'
 require 'mocha'
 require 'travis/config'
 
+module Travis::Test
+  class Config < Travis::Config
+    define amqp: { username: 'guest', password: 'guest', host: 'localhost', prefetch: 1 }
+    define database: { adapter: 'postgresql', database: 'test', encoding: 'unicode' }
+  end
+end
+
 RSpec.configure do |config|
   config.mock_with :mocha
 end
---
+++ spec/travis/config/docker_spec.rb
@@ -0,0 +1,37 @@
+describe Travis::Config::Docker do
+  let(:config) { Travis::Test::Config.load(:docker) }
+  let(:vars)   { %w(POSTGRESQL_PORT RABBITMQ_PORT REDIS_PORT) }
+  after        { vars.each { |key| ENV.delete(key) } }
+
+  describe 'loads POSTGRESQL_PORT to config.database' do
+    before { ENV['POSTGRESQL_PORT'] = 'tcp://172.17.0.11:5432' }
+
+    it 'loads host and port from the env var' do
+      expect(config.database.values_at(:host, :port)).to eq(['172.17.0.11', '5432'])
+    end
+
+    it 'keeps adapter, database, encoding from the regular config' do
+      expect(config.database.values_at(:adapter, :database, :encoding)).to eq(['postgresql', 'test', 'unicode'])
+    end
+  end
+
+  describe 'loads RABBITMQ_PORT to config.amqp' do
+    before { ENV['RABBITMQ_PORT'] = 'tcp://172.17.0.11:5672' }
+
+    it 'loads host and port from the env var' do
+      expect(config.amqp.values_at(:host, :port)).to eq(['172.17.0.11', '5672'])
+    end
+
+    it 'keeps username, password, prefetch from the regular config' do
+      expect(config.amqp.values_at(:username, :password, :prefetch)).to eq(['guest', 'guest', 1])
+    end
+  end
+
+  describe 'loads REDIS_PORT' do
+    before { ENV['REDIS_PORT'] = 'tcp://172.17.0.7:6379' }
+
+    it 'loads the port to redis.url' do
+      expect(config.redis).to eq({ url: 'tcp://172.17.0.7:6379' })
+    end
+  end
+end
