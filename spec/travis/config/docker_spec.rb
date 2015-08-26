6a1e8cf3059dfbc4c05c761f662085297dab8f33
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-08-26 14:39:43 +0200

    fix(docker): use "redis" schema in redis url

--- spec/travis/config/docker_spec.rb
+++ spec/travis/config/docker_spec.rb
@@ -30,8 +30,8 @@ describe Travis::Config::Docker do
   describe 'loads REDIS_PORT' do
     before { ENV['REDIS_PORT'] = 'tcp://172.17.0.7:6379' }
 
-    it 'loads the port to redis.url' do
-      expect(config.redis.to_h).to eq({ url: 'tcp://172.17.0.7:6379' })
+    it 'loads the port to redis.url as a redis:// url' do
+      expect(config.redis.to_h).to eq({ url: 'redis://172.17.0.7:6379' })
     end
   end
 end
