2367f59e9f72b7640730887d335e8f521c507fec
Dan Buch <daniel.buch@gmail.com>
2015-09-10 23:28:05 -0400

    Accept logs database from env
    so that attached database URLs may be used to ensure failover recovery

--- spec/travis/config/heroku_spec.rb
+++ spec/travis/config/heroku_spec.rb
@@ -3,6 +3,7 @@ describe Travis::Config::Heroku do
   let(:vars)   { %w(DATABASE_URL POOL_SIZE) }
   after        { vars.each { |key| ENV.delete(key) } }
   before       { ENV['DATABASE_URL'] = 'postgres://username:password@hostname:port/database' }
+  before       { ENV['LOGS_DATABASE_URL'] = 'postgres://username:password@hostname:port/logs_database' }
 
   it 'loads a DATABASE_URL with a port' do
     expect(config.database.to_hash).to eq(
@@ -52,6 +53,61 @@ describe Travis::Config::Heroku do
       host:     'hostname',
       port:     'port',
       database: 'database',
+      username: 'username',
+      password: 'password',
+      encoding: 'unicode',
+      pool:     25
+    )
+  end
+
+  it 'loads a LOGS_DATABASE_URL with a port' do
+    expect(config.logs_database.to_hash).to eq(
+      adapter:  'postgresql',
+      host:     'hostname',
+      port:     'port',
+      database: 'logs_database',
+      username: 'username',
+      password: 'password',
+      encoding: 'unicode'
+    )
+  end
+
+  it 'loads a LOGS_DATABASE_URL without a port' do
+    ENV['LOGS_DATABASE_URL'] = 'postgres://username:password@hostname/logs_database'
+
+    expect(config.logs_database.to_hash).to eq(
+      adapter:  'postgresql',
+      host:     'hostname',
+      database: 'logs_database',
+      username: 'username',
+      password: 'password',
+      encoding: 'unicode'
+    )
+  end
+
+  it 'loads LOGS_DB_POOL' do
+    ENV['LOGS_DB_POOL'] = '25'
+
+    expect(config.logs_database.to_hash).to eq(
+      adapter:  'postgresql',
+      host:     'hostname',
+      port:     'port',
+      database: 'logs_database',
+      username: 'username',
+      password: 'password',
+      encoding: 'unicode',
+      pool:     25
+    )
+  end
+
+  it 'loads LOGS_DATABASE_POOL_SIZE' do
+    ENV['LOGS_DATABASE_POOL_SIZE'] = '25'
+
+    expect(config.logs_database.to_hash).to eq(
+      adapter:  'postgresql',
+      host:     'hostname',
+      port:     'port',
+      database: 'logs_database',
       username: 'username',
       password: 'password',
       encoding: 'unicode',
