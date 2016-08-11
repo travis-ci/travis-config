2cc7ba49a6bb0494f34ff4a1db8d1820b7e92357
bnferguson <bnferguson@gmail.com>
2016-08-11 13:45:02 +0200

    fix typo

--- lib/travis/config/heroku/url.rb
+++ lib/travis/config/heroku/url.rb
@@ -25,7 +25,7 @@ module Travis
 
         # The amqps:// protocol needs to also pass along that it has SSL enabled
         # for adapters such as march_hare
-        class Ampqs < Ampq
+        class Ampqs < Amqp
           def ssl
             true
           end
